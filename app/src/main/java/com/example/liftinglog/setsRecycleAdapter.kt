551e99d70d146bbb9f448a5af22adee431ca25d1
Tobias Claesson <tobbe.classon@hotmail.se>
2020-04-30 11:10:14 +0200

    set_list_item editable

--- app/src/main/java/com/example/liftinglog/setsRecycleAdapter.kt
+++ app/src/main/java/com/example/liftinglog/setsRecycleAdapter.kt
@@ -1,10 +1,14 @@
 package com.example.liftinglog
 
 import android.content.Context
+import android.text.Editable
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.TextView
+import androidx.core.widget.addTextChangedListener
 import androidx.recyclerview.widget.RecyclerView
 
 class SetsRecycleAdapter(private val context: Context,
@@ -15,8 +19,22 @@ class SetsRecycleAdapter(private val context: Context,
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         val setText = itemView.findViewById<TextView>(R.id.setNumberText)
-        val repText = itemView.findViewById<TextView>(R.id.repNumberText)
-        val weightText = itemView.findViewById<TextView>(R.id.weightNumberText)
+        val repText = itemView.findViewById<EditText>(R.id.repNumberText)
+        val weightText = itemView.findViewById<EditText>(R.id.weightNumberText)
+
+        init {
+            repText.setOnFocusChangeListener { v, hasFocus ->
+                if (hasFocus){
+                    Log.d("!!!", "focuuus")
+                }
+                else{
+                    val data = repText.text
+
+                    exercise.reps = data.toString().toInt()
+                }
+
+            }
+        }
 
     }
 
@@ -32,7 +50,7 @@ class SetsRecycleAdapter(private val context: Context,
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.setText.text = (position + 1).toString()
-        holder.repText.text = exercise.reps.toString()
-        holder.weightText.text = exercise.weight.toString()
+        holder.repText.setText(exercise.reps.toString())
+        holder.weightText.setText(exercise.weight.toString())
     }
 }
